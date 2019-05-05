dde8dfeda9bf199ca3a3f5b5522ea0337d677062
KenHua Liew <l_kenhua93@hotmail.com>
2019-05-05 19:45:46 +0800

    can solve iterative

--- sudoku_reader.py
+++ sudoku_reader.py
@@ -1,17 +1,30 @@
 import sudoku_util
+from sudoku_util import SudokuPuzzle
+from time import sleep
 
 def main():
 	example = "093001600600000079470690000360000700700502001002000043000026037130000006006300150"
+	# example = "403072860701580300200106500000050410000000000037060000002807003004015207075620108"
 	puzzle = convert_to_puzzle(example)
-	print_sudoku(example)
+
+	print_sudoku(puzzle.solving_frame)
+
+	ite = 10
+	while(sudoku_util.has_certain_possibilities(puzzle.possibilities) and ite > 0):
+		sleep(1)
+		print(ite)
+		puzzle.fill_up_certain_ones()
+		print_sudoku(puzzle.solving_frame)
+		ite = ite - 1
+
 
 def convert_to_puzzle(data):
-	return sudoku_util.SudokuPuzzle(data)
+	return SudokuPuzzle(data)
 
 def print_sudoku(puzzle):
 	for x in range(9):
 		for y in range(9):
-			if puzzle[x*9 + y] == '0':
+			if puzzle[x * 9 + y] == '0':
 				print('-', end='')
 			else:
 				print(puzzle[x*9 + y], end='')
--- sudoku_util.py
+++ sudoku_util.py
@@ -1,8 +1,8 @@
+from collections import defaultdict
 # SUDOKU DIMENSION
 WIDTH = 3
 SQUARE = 9
 NUMBERSET = {1,2,3,4,5,6,7,8,9}
-# SQUARE_HASH = { 0:0, 1:3, 2:6, 3:27, 4:30, 5:33, 6:54, 7:57, 8:60 } #hard coded
 
 class SudokuPuzzle:
 	# in grid form where x is row, y is col
@@ -15,33 +15,58 @@ class SudokuPuzzle:
 	# }
     def __init__(self, data):
         self.main_frame = data
-        self.possibilities = {}
+        self.solving_frame = data
+        self.possibilities = self.set_possibilities(self.main_frame)
 
+    # SETUP
     def get_row_numbers(self, row):
-        row_ = set(self.main_frame[(row * SQUARE):(row * SQUARE) + SQUARE])
-        row_.discard('0')
+        row_ = set([int(i) for i in self.solving_frame[(row * SQUARE):(row * SQUARE) + SQUARE]])
+        row_.discard(0)
         return row_
 
     def get_col_numbers(self, col):
-        col_ = set(self.main_frame[col:SQUARE ** 2:SQUARE])
-        col_.discard('0')
+        col_ = set([int(i) for i in self.solving_frame[col:SQUARE ** 2:SQUARE]])
+        col_.discard(0)
         return col_
 
     def get_sq_numbers(self, sq):
         start = (int(sq/WIDTH) * WIDTH * SQUARE) + (sq % WIDTH) * WIDTH
-        sq_ = set(self.main_frame[start:start + WIDTH])
-        sq_ = sq.union(set(self.main_frame[start + SQUARE:start + SQUARE + WIDTH]), set(self.main_frame[start + SQUARE * 2:start + SQUARE * 2 + WIDTH]))
-        sq_.discard('0')
+        sq_ = set([int(i) for i in self.solving_frame[start:start + WIDTH]])
+        sq_ = sq_.union(set([int(i) for i in self.solving_frame[start + SQUARE:start + SQUARE + WIDTH]]))
+        sq_ = sq_.union(set([int(i) for i in self.solving_frame[start + SQUARE * 2:start + SQUARE * 2 + WIDTH]]))
+        sq_.discard(0)
         return sq_
+
+    def set_possibilities(self, data):
+        possibilities = defaultdict(dict)
+        for x in range(SQUARE):
+            for y in range(SQUARE):
+                if data[x * SQUARE + y] == '0':
+                    r,c,s = get_cell_location(x * SQUARE + y)
+                    r,c,s = self.get_row_numbers(r), self.get_col_numbers(c), self.get_sq_numbers(s)
+                    r,c,s = find_missing(r), find_missing(c), find_missing(s)
+                    possibilities[x][y] = intersect_sets(r, c, s)
+        return possibilities
+
+    # SOLVE
+    def fill_up_certain_ones(self):
+        temp = self.solving_frame
+        for x, set_ in self.possibilities.items():
+            # delete = [y for (y, val) in set_.items() if len(val) == 1]
+            for y, val in set_.items():
+                if len(val) == 1:
+                    temp = temp[:x * SQUARE + y] + str(val.pop()) + temp[x * SQUARE + y + 1:]
+            # for y in delete: del set_[y]
+        self.solving_frame = temp
+        self.possibilities = self.set_possibilities(self.solving_frame)
 
 def get_cell_location(ordinal):
     # assume this is 9 x 9 sudoku
     # ordinal in terms of main_frame order
-    row = int(ordinal / 9)
-    col = int(ordinal % 9)
-    square = int(row/3) * 3 + int(col/3)
+    row = int(ordinal / SQUARE)
+    col = int(ordinal % SQUARE)
+    square = int(row / WIDTH) * WIDTH + int(col / WIDTH)
     return row, col, square
-
 
 def find_missing(num_set):
     missing = NUMBERSET.difference(num_set)
@@ -51,12 +76,19 @@ def intersect_sets(horizontal_set, vertical_set, square_set):
     subset = horizontal_set.intersection(vertical_set, square_set)
     return subset
 
+# SOLVE
+def has_certain_possibilities(possible_dict):
+    for x, set_ in possible_dict.items():
+        for y, val in set_.items():
+            if len(val) == 1:
+                return True
+    return False
+
 def main():
     # testing
-    example = "093001600600000079470690000360000700700502001002000043000026037130000006006300150"
+    # example = "093001600600000079470690000360000700700502001002000043000026037130000006006300150"
+    example = "403072860701580300200106500000050410000000000037060000002807003004015207075620108"
     puzzle = SudokuPuzzle(example)
-    for i in range(81):
-        row, col, sq = get_cell_location(i)
-        print(puzzle.get_col_numbers(col))
+    print(puzzle.possibilities)
 
-main()
+# main()
